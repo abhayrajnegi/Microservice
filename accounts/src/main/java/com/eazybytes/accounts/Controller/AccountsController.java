be1dc440d7842081d4071f9a0a5308037e2f80af
abhayrajnegi <abhayrajnegi0416@gmail.com>
2025-02-22 10:30:10 +0530

    building accounts , cards and loans microservice is done

--- accounts/src/main/java/com/eazybytes/accounts/Controller/AccountsController.java
+++ accounts/src/main/java/com/eazybytes/accounts/Controller/AccountsController.java
@@ -3,8 +3,15 @@ package com.eazybytes.accounts.Controller;
 import com.eazybytes.accounts.Entity.Customer;
 import com.eazybytes.accounts.constants.AccountConstants;
 import com.eazybytes.accounts.dto.CustomerDto;
+import com.eazybytes.accounts.dto.ErrorResponseDto;
 import com.eazybytes.accounts.dto.ResponseDto;
 import com.eazybytes.accounts.service.IAccountsService;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.media.Content;
+import io.swagger.v3.oas.annotations.media.Schema;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import jakarta.validation.Valid;
 import jakarta.validation.constraints.Pattern;
 import lombok.AllArgsConstructor;
@@ -14,6 +21,10 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.*;
 
+@Tag(
+      name = "CRUD REST API's for Accounts in EazyBank",
+      description = "CRUD REST API's for Accounts in EazyBank to create ,update and delete account details"
+)
 @RestController
 @RequestMapping(path = "/api",produces = {MediaType.APPLICATION_JSON_VALUE})
 @AllArgsConstructor
@@ -23,6 +34,14 @@ public class AccountsController {
     private IAccountsService iAccountsService;
 
     @PostMapping(path="/create")
+    @Operation(
+            summary = "Create Account REST API",
+            description = "REST API to create new Customer and account inside EazyBank"
+    )
+    @ApiResponse(
+            responseCode = "201",
+            description = "HTTP Status Created"
+    )
     public ResponseEntity<ResponseDto>createAccount(@RequestBody @Valid CustomerDto customerDto) {
         iAccountsService.createAccount(customerDto);
         return ResponseEntity
@@ -32,12 +51,39 @@ public class AccountsController {
 
 
     @GetMapping("/fetch")
+    @Operation(
+            summary = "Fetch Account REST API",
+            description = "REST API to fetch account details based on a mobile number"
+    )
     public ResponseEntity<CustomerDto>fetchAccountDetails(@RequestParam @Pattern(regexp = "(^$|[0-9]{10})") String mobileNumber){
         CustomerDto customerDto=iAccountsService.fetchAccount(mobileNumber);
         return ResponseEntity.status(HttpStatus.OK).body(customerDto);
     }
 
     @PutMapping("/update")
+    @Operation(
+            summary = "Update Account REST API",
+            description = "REST API to update account details based on a mobile number"
+    )
+    @ApiResponses({
+        @ApiResponse(
+                responseCode = "200",
+                description = "HTTP Status OK"
+        ),
+        @ApiResponse(
+                responseCode = "500",
+                description = "HTTP Status Internal Server Error",
+                content = @Content(
+                schema = @Schema(implementation = ErrorResponseDto.class)
+                )
+        ),
+
+            @ApiResponse(
+                    responseCode = "417",
+                    description = "EXpectation failed"
+            )
+
+    })
     public ResponseEntity<ResponseDto>updateAccountDetails(@RequestBody CustomerDto customerDto) {
 
         boolean isUpdated=iAccountsService.updateAccount(customerDto);
@@ -48,12 +94,34 @@ public class AccountsController {
         }else {
             return ResponseEntity
                     .status(HttpStatus.INTERNAL_SERVER_ERROR)
-                    .body(new ResponseDto(AccountConstants.STATUS_500,AccountConstants.MESSAGE_500));
+                    .body(new ResponseDto(AccountConstants.STATUS_417,AccountConstants.MESSAGE_417_UPDATE));
         }
 
     }
 
     @DeleteMapping("/delete")
+    @Operation(
+            summary = "Delete Account REST API",
+            description = "REST API to delete account details based on a mobile number"
+    )
+    @ApiResponses({
+        @ApiResponse(
+                responseCode = "200",
+                description = "HTTP Status OK"
+        ),
+            @ApiResponse(
+                    responseCode = "500",
+                    description = "HTTP Status Internal Server Error",
+                    content = @Content(
+                            schema = @Schema(implementation = ErrorResponseDto.class)
+                    )
+            ),
+
+            @ApiResponse(
+                    responseCode = "417",
+                    description = "EXpectation failed"
+            )
+    })
     public ResponseEntity<ResponseDto>deleteAccountDetails(@RequestParam  @Pattern(regexp = "(^$|[0-9]{10})") String mobileNumber){
         boolean isDeleted=iAccountsService.deleteAccount(mobileNumber);
         if(isDeleted){
@@ -63,7 +131,7 @@ public class AccountsController {
         }else {
             return ResponseEntity
                     .status(HttpStatus.INTERNAL_SERVER_ERROR)
-                    .body(new ResponseDto(AccountConstants.STATUS_500,AccountConstants.MESSAGE_500));
+                    .body(new ResponseDto(AccountConstants.STATUS_417,AccountConstants.MESSAGE_417_DELETE));
         }
     }
 
--- accounts/src/main/java/com/eazybytes/accounts/dto/AccountsDto.java
+++ accounts/src/main/java/com/eazybytes/accounts/dto/AccountsDto.java
@@ -1,21 +1,39 @@
 package com.eazybytes.accounts.dto;
 
+import io.swagger.v3.oas.annotations.media.Schema;
 import jakarta.validation.constraints.Digits;
 import jakarta.validation.constraints.NotEmpty;
 import jakarta.validation.constraints.Pattern;
 import lombok.Data;
 
 @Data
+@Schema(
+        name = "Accounts",
+        description = "Schema to hold Account information"
+)
 public class AccountsDto {
 
     @NotEmpty(message = "Account Number can not be empty")
     @Pattern(regexp = "(^$|[0-9]{10})",message = "Account Number should be 10 digits")
+    @Schema(
+            description = "Account number of Eazy Bank Account",
+            example = "3455433465"
+    )
     private Long accountNumber;;
 
     @NotEmpty(message = "Account Type can not be empty")
+    @Schema(
+            description ="Accounts type of Eazy Bank Account",
+            example = "Savings"
+    )
     private String accountType;
 
-    @NotEmpty(message = "Branch Address can not be empty")
+    @Schema(
+            description = "Eazy Bank branch address",
+            example = "123 New york"
+    )
+    @NotEmpty(message = "Branch Address can not be empty"
+    )
     private String branchAddress;
 
 
